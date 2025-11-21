60af43035371fa7d6bafcd0710b2aec90ae0d67c
machu001 <maciek.mendrygal@gmail.com>
2025-11-21 15:03:47 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import RoomDetails from '../components/RoomDetails.jsx';
+import RoomDetails from './components/RoomDetails.jsx';
 import "./App.css";
 
 const API = "https://rezerwacje-sal-backend.onrender.com";
